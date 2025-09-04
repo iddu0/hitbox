6d4c8f7f76ad641004a58a724958bdff8d5ba2e6
iddu01 <157660303+iddu0@users.noreply.github.com>
2025-09-03 20:54:42 -0400

    Update book.js

--- client/book.js
+++ client/book.js
@@ -1,13 +1,13 @@
 (async function(){
     var jselem = document.createElement('script');
-    await fetch('./cdn/script.js')
+    await fetch('https://teamhitbox.github.io/hitbox/client/cdn/script.js')
 	.then((response) => response.text())
 	.then((data) => {
 		jselem.innerHTML = data;
 	});
     document.body.appendChild(jselem);
     var csselem = document.createElement('style');
-    await fetch('./cdn/style.css')
+    await fetch('https://teamhitbox.github.io/hitbox/client/cdn/style.css')
 	.then((response) => response.text())
 	.then((data) => {
 		csselem.innerHTML = data;
